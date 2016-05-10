0a1b5c3ce62da645510c7f568f6f5eadc83be283
S.M Zubair <Devilhunter341@gmail.com>
2016-05-10 18:25:08 +0500

    eden xml completed

--- app/src/main/java/com/example/android/justjava2/ScrollingActivity.java
+++ app/src/main/java/com/example/android/justjava2/ScrollingActivity.java
@@ -22,11 +22,15 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "It will do nothing :p", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
